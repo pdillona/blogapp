f9c82f352c3b23d8b3fbdd8601c0c7973ec36880
ssar <ssar@nate.com>
2021-09-08 14:52:34 +0900

    모델생성완료(join)

--- src/main/java/com/cos/blogapp/domain/board/Board.java
+++ src/main/java/com/cos/blogapp/domain/board/Board.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.cos.blogapp.domain.user.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,7 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Entity
+@Entity //테이블만들어주는데 어플리케이션yml에서 create해야 만들어진다.
 public class Board {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +25,9 @@ public class Board {
 	private int id; 					 
 	private String title; 
 	private String content; 
+	
+	@JoinColumn(name="userId")
+	@ManyToOne
+	private User user;	//s
+	
 }
--- src/main/webapp/WEB-INF/views/layout/footer.jsp
+++ src/main/webapp/WEB-INF/views/layout/footer.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
     
-
+<br/>
 <div class="jumbotron text-center" style="margin-bottom:0">
   <p>pdillon</p>
   <p>📞010-1234-5678</p>
